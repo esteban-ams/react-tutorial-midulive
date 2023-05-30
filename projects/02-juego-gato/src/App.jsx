770484cd0805c0e79ec770a1c9ebceebef450798
esteban-ams <esteban.ams95@gmail.com>
2023-05-30 12:10:02 -0400

    new Gato Game project

---
+++ projects/02-juego-gato/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+      </div>
+  )
+}
+
+
+function App() {
+  const [ board, setBoard ] = useState(Array(9).fill(null))
+  const [ turn, setTurn ] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  }
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+          </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+
+      </section>
+    </main>
+  )
+}
+
+export default App
