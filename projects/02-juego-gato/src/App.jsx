ee8360d31b9b8d9b296090b9458a8ce9e03f01e9
esteban-ams <esteban.ams95@gmail.com>
2023-05-30 13:07:17 -0400

    stop the game nad new winner ready

--- projects/02-juego-gato/src/App.jsx
+++ projects/02-juego-gato/src/App.jsx
@@ -10,7 +10,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
   return (
     <div onClick={handleClick} className={className}>
@@ -23,9 +23,52 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 function App() {
   const [ board, setBoard ] = useState(Array(9).fill(null))
   const [ turn, setTurn ] = useState(TURNS.X)
+  const [ winner, setWinner ] = useState(null) // null no hay ganador, false es que hay empate
 
-  const updateBoard = () => {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of winningCombinations) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]) {
+        setWinner(boardToCheck[a])
+        return true
+      }
+    }
+    // si no hay ganador
+    return false
+
+  }
+
+
+  const updateBoard = (index) => {
+    // no actualiza el estado si ya hay un valor
+    if (board[index] || winner) return
+    // actualiza el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambia el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+
   }
   return (
     <main className="board">
