d82ee2b871acbcb186ac48bba61a770518b30227
esteban-ams <esteban.ams95@gmail.com>
2023-05-31 20:37:43 -0400

    confetti y mejora de componentes

--- projects/02-juego-gato/src/App.jsx
+++ projects/02-juego-gato/src/App.jsx
@@ -1,23 +1,11 @@
 import { useState } from "react"
+import confetti from "canvas-confetti"
 
-const TURNS = {
-  X: 'X',
-  O: 'O'
-}
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
 
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-      </div>
-  )
-}
 
 
 function App() {
@@ -25,32 +13,6 @@ function App() {
   const [ turn, setTurn ] = useState(TURNS.X)
   const [ winner, setWinner ] = useState(null) // null no hay ganador, false es que hay empate
 
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-
-  const checkWinner = (boardToCheck) => {
-    for (const combo of winningCombinations) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    // si no hay ganador
-    return false
-
-  }
 
   const resetGame = () => {
      setBoard(Array(9).fill(null))
@@ -58,11 +20,6 @@ function App() {
      setWinner(null)
   }
 
-
-  const checkEndGame = (boardToCheck) => {
-    // revisa si hay empate
-    return boardToCheck.every(square => square !== null)
-  }
 
   const updateBoard = (index) => {
     // no actualiza el estado si ya hay un valor
@@ -75,8 +32,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     // revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
       setWinner(false)
@@ -111,29 +69,12 @@ function App() {
         </Square>
 
       </section>
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                    ? 'Empate'
-                    : 'Ganador:'
-                }
-              </h2>
 
-              <header className="win">
-                {winner && <Square>{winner}</Square>}
-              </header>
+      <WinnerModal
+        winner={winner}
+        resetGame={resetGame}
+      />
 
-              <footer>
-                <button onClick={resetGame}> Empezar de nuevo </button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
     </main>
   )
 }
---
+++ projects/02-juego-gato/src/logic/board.js
@@ -0,0 +1,22 @@
+import { winningCombinations } from '../constants.js'
+
+export const checkWinnerFrom = (boardToCheck) => {
+    for (const combo of winningCombinations) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+
+  }
+
+export const checkEndGame = (boardToCheck) => {
+    // revisa si hay empate
+    return boardToCheck.every(square => square !== null)
+  }
